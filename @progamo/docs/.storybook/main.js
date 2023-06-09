befdc7e87c7896eb23b6a8855d4df5172932cc5a
Manoel Neto <maguserv@gmail.com>
2023-06-09 17:46:40 -0300

    chore: add config baseUrl to production

--- @progamo/docs/.storybook/main.js
+++ @progamo/docs/.storybook/main.js
@@ -13,5 +13,11 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+	viteFinal: (config, { configType }) => {
+		if (configType === 'PRODUCTION') {
+			config.base = '/progamo-ui/';
+		}
+		return config;
+	}
 };
 export default config;
